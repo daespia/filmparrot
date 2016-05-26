0ba0532d479db3dd168183b73b662a34bf5a447f
Raúl Hernáez <rhorbul@gmail.com>
2016-05-27 00:38:19 +0200

    Aplicación terminada.
    Signed-off-by: Raúl Hernáez <rhorbul@gmail.com>

--- FilmParrot02/app/src/main/java/filmparrot/movil/informatica/filmparrot/PrincipalFragment.java
+++ FilmParrot02/app/src/main/java/filmparrot/movil/informatica/filmparrot/PrincipalFragment.java
@@ -3,11 +3,15 @@ package filmparrot.movil.informatica.filmparrot;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.pm.ActivityInfoCompat;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
 import java.util.Random;
@@ -15,10 +19,13 @@ import java.util.Random;
 import filmparrot.movil.informatica.filmparrot.auxiliar.Utils;
 import filmparrot.movil.informatica.filmparrot.logica.Pelicula;
 import filmparrot.movil.informatica.filmparrot.logica.Persona;
+import filmparrot.movil.informatica.filmparrot.logica.Serie;
 
 
 public class PrincipalFragment extends Fragment {
-    private Pelicula elemento;
+    private Pelicula elementoPelicula;
+    private Serie elementoSerie;
+    private CardView card_gotit, card_film, card_serie;
 
 
     public PrincipalFragment() {
@@ -31,72 +38,70 @@ public class PrincipalFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_principal, container, false);
 
-        Random ran = new Random();
-        int k = (int)(ran.nextDouble() * Utils.fachada.getPeliculas().size());
-        elemento = (Pelicula)Utils.fachada.getElementoPorId(Utils.fachada.getPeliculas().get(k).getId());
+        TextView got_it = (TextView) view.findViewById(R.id.gotit_text);
+        card_gotit = (CardView) view.findViewById(R.id.welcome_card);
+        card_film = (CardView) view.findViewById(R.id.film_card);
+        card_serie = (CardView) view.findViewById(R.id.serie_card);
 
+        elementoPelicula = Utils.fachada.getPeliculas().get(new Random().nextInt(5));
+        elementoSerie = Utils.fachada.getSeries().get(new Random().nextInt(3));
+
+        // Película.
         ImageView imageCover = (ImageView) view.findViewById(R.id.coverImage);
-        imageCover.setImageResource(elemento.getImagen());
+        imageCover.setImageResource(elementoPelicula.getImagen());
 
         TextView pointAverage = (TextView) view.findViewById(R.id.pointAverage);
-        pointAverage.setBackgroundColor(Utils.getProgressiveColor(elemento.getMedia(), getActivity().getApplicationContext()));
-        pointAverage.setText(String.valueOf(elemento.getMedia()));
-
         TextView pointsLabel = (TextView) view.findViewById(R.id.pointsLabel);
-        pointsLabel.setText(elemento.getPuntuaciones().size() + " votos");
-
         TextView reviewsLabel = (TextView) view.findViewById(R.id.reviewsLabel);
-        reviewsLabel.setText(elemento.getNumCriticas() + " críticas");
-        reviewsLabel.setOnClickListener(new View.OnClickListener() {
+
+        pointAverage.setBackgroundColor(Utils.getProgressiveColor(elementoPelicula.getMedia(), getActivity()));
+        pointAverage.setText(String.valueOf(elementoPelicula.getMedia()));
+        pointsLabel.setText(elementoPelicula.getPuntuaciones().size() + " votos");
+        reviewsLabel.setText(elementoPelicula.getNumCriticas() + " críticas");
+        ((TextView) view.findViewById(R.id.elementLabel)).setText(elementoPelicula.getTitulo());
+        ((TextView) view.findViewById(R.id.descriptionText)).setText(elementoPelicula.getDescripcion());
+
+        //Serie.
+        ImageView imageSerieCover = (ImageView) view.findViewById(R.id.coverSerieImage);
+        imageSerieCover.setImageResource(elementoSerie.getImagen());
+
+        TextView pointSerieAverage = (TextView) view.findViewById(R.id.pointSerieAverage);
+        TextView pointsSerieLabel = (TextView) view.findViewById(R.id.pointsSerieLabel);
+        TextView reviewsSerieLabel = (TextView) view.findViewById(R.id.reviewsSerieLabel);
+
+        pointSerieAverage.setBackgroundColor(Utils.getProgressiveColor(elementoSerie.getMedia(), getActivity()));
+        pointSerieAverage.setText(String.valueOf(elementoPelicula.getMedia()));
+        pointsSerieLabel.setText(elementoSerie.getPuntuaciones().size() + " votos");
+        reviewsSerieLabel.setText(elementoSerie.getNumCriticas() + " críticas");
+        ((TextView) view.findViewById(R.id.elementSerieLabel)).setText(elementoSerie.getTitulo());
+        ((TextView) view.findViewById(R.id.descriptionSerieText)).setText(elementoSerie.getDescripcion());
+
+        got_it.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity().getApplicationContext(), AllReviewsActivity.class);
-                intent.putExtra("id", elemento.getId());
-                startActivity(intent);
+                card_gotit.setVisibility(View.GONE);
             }
         });
 
-        ((TextView) view.findViewById(R.id.descriptionText)).setText(elemento.getDescripcion());
-        ((TextView) view.findViewById(R.id.countryText)).setText(elemento.getPais());
-        ((TextView) view.findViewById(R.id.elementLabel)).setText("Película del día: "+elemento.getTitulo());
-
-        TextView duracion  = (TextView) view.findViewById(R.id.durationText);
-        duracion.setText("" + elemento.getDuracion());
-
-        TextView genero  = (TextView) view.findViewById(R.id.genreText);
-        genero.setText(elemento.getGenero());
-
-        TextView anio  = (TextView) view.findViewById(R.id.yearText);
-        Calendar c = Calendar.getInstance();
-        c.setTime(elemento.getFechaEstreno());
-        anio.setText("" + c.get(Calendar.YEAR));
-
-        TextView bso  = (TextView) view.findViewById(R.id.BsoText);
-        //bso.setText(e.getBso().getTitulo());
-
-        TextView director  = (TextView) view.findViewById(R.id.directorText);
-        String directores = "";
-
-        if(elemento.getDirectores()==null){
-            director.setText("Desconocido");
-        }else{
-            for(Persona p: elemento.getDirectores()){
-                directores+=p.getTitulo() +", ";
+        card_serie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ElementViewActivity.class);
+                intent.putExtra("id", elementoSerie.getId());
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.left_toright, R.anim.right_toleft);
             }
-            director.setText(directores);
-        }
+        });
 
-        TextView actor  = (TextView) view.findViewById(R.id.actorText);
-        String actores = "";
-
-        if(elemento.getActores()==null){
-            actor.setText("Desconocido");
-        }else{
-            for(Persona p: elemento.getActores()){
-                actores+= p.getTitulo() + ", ";
+        card_film.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ElementViewActivity.class);
+                intent.putExtra("id", elementoPelicula.getId());
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.left_toright, R.anim.right_toleft);
             }
-            actor.setText(actores);
-        }
+        });
 
         return view;
 
